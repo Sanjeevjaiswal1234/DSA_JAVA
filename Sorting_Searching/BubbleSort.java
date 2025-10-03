c3f8012e7e8a91c73eaefc6e0ac1dac3c544fa71
Sanjeevjaiswal1234 <129573943+Sanjeevjaiswal1234@users.noreply.github.com>
2025-10-04 00:13:32 +0530

    Update BubbleSort.java

--- Sorting_Searching/BubbleSort.java
+++ Sorting_Searching/BubbleSort.java
@@ -1,1 +1,28 @@
-
+class BubbleSort{
+	public static void main(String[] args){
+		int[] a={12, 8, 15, 18, 10, 16, 11};
+		System.out.println("====Before Sorting====");
+		for(int x:a)
+			System.out.print(x+" ");
+		bubbleSort(a);	
+		System.out.println("\n====After Sorting====");
+		for(int x:a)
+			System.out.print(x+" ");	
+	}
+	public static void bubbleSort(int[] a){
+		int n=a.length-1;
+		for(int i=0;i<n;i++){
+			boolean flag=true;
+			for(int j=0;j<n-i;j++){
+				if(a[j]>a[j+1]){
+					int temp=a[j];
+					a[j]=a[j+1];
+          a[j+1]=temp;
+				flag=false;
+				}
+			}
+			if(flag)
+				return;
+		}
+	}
+}
